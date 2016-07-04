a6666f9860d74a8213b2f63ca9a666f9752b87d6
Bevis <bevis_chen@soft-world.com.tw>
2016-07-04 15:43:08 +0800

    add some annotation

--- TestHTMLStringOnTextView/UITextView+ShowHTMLString.h
+++ TestHTMLStringOnTextView/UITextView+ShowHTMLString.h
@@ -10,6 +10,12 @@
 
 @interface UITextView (ShowHTMLString)
 
+/*
+ // If YES, Translate Html String to Text
+ &lt;p&gt;&lt;font color='#006400'&gt;&lt;u&gt;&lt;strike&gt;&lt;i&gt;說明頁測試&lt;/i&gt;&lt;/strike&gt;&lt;/u&gt;&lt;/font&gt;&lt;/p&gt;&lt;br&gt;
+ 
+ <p><font color='#006400'><u><strike><i>說明頁測試</i></strike></u></font></p><br>
+*/
 - (NSMutableAttributedString *)decodeHtmlString:(NSString *)htmlString stringByConvertingHTMLToPlainText:(BOOL)yesOrNo;
 -(void)setAttributedTextAndDecodeHtmlString:(NSString*)htmlString stringByConvertingHTMLToPlainText:(BOOL)yesOrNo;
 
--- TestHTMLStringOnTextView/UITextView+ShowHTMLString.m
+++ TestHTMLStringOnTextView/UITextView+ShowHTMLString.m
@@ -14,12 +14,6 @@
 
 - (NSMutableAttributedString *)decodeHtmlString:(NSString *)htmlString stringByConvertingHTMLToPlainText:(BOOL)yesOrNo {
     
-    /*
-     &lt;p&gt;&lt;font color='#006400'&gt;&lt;u&gt;&lt;strike&gt;&lt;i&gt;說明頁測試&lt;/i&gt;&lt;/strike&gt;&lt;/u&gt;&lt;/font&gt;&lt;/p&gt;&lt;br&gt;
-     
-     <p><font color='#006400'><u><strike><i>說明頁測試</i></strike></u></font></p><br>
-     */
-    // If YES, Translate Html String to Text
     if (yesOrNo) {
         htmlString = [htmlString stringByConvertingHTMLToPlainText];
     }
